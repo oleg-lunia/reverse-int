7b6e974848b8e4494dc97f207002c337ba4642b7
Oleg Lunia <oleglunja2@gmail.com>
2021-03-15 14:44:23 +0300

    refactoring: changed the file index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  n = String(n);
+  let arr = n.split('');
+  if (arr[0] == '-') {
+    arr.shift();
+  }
+  arr.reverse();
+  let newN = arr.join('');
+  return newN;
 }
